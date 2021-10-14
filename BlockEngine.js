1a6beceb22166fdc295308b84276bfbea595cace
Vladislav <vladgrz40@gmail.com>
2021-10-15 02:40:28 +0300

    Another fix for hoes

--- BlockEngine.js
+++ BlockEngine.js
@@ -1203,6 +1203,8 @@ var ToolType;
         onItemUse: function (coords, item, block, player) {
             if ((block.id == 2 || block.id == 3) && coords.side != 0) {
                 var region = WorldRegion.getForActor(player);
+                if (region.getBlockId(coords.x, coords.y + 1, coords.z) != 0)
+                    return;
                 region.setBlock(coords, 60, 0);
                 region.playSound(coords.x + .5, coords.y + 1, coords.z + .5, "step.gravel", 1, 0.8);
                 item.applyDamage(1);
