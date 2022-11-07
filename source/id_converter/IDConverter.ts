02b47771e407ccc9c2b14515eea6fefa5bc15774
MineExplorer <vladgrz40@gmail.com>
2022-11-07 13:28:58 +0300

    IDConverter docs

--- source/id_converter/IDConverter.ts
+++ source/id_converter/IDConverter.ts
@@ -1,4 +1,7 @@
 // By NikolaySavenko (https://github.com/NikolaySavenko)
+/**
+ * Module to convert item ids depending on the game version.
+ */
 namespace IDConverter {
 	type IDDataPair = {
 		id: number;
@@ -7,10 +10,21 @@ namespace IDConverter {
 
 	const oldIDPairs: {[key: string]: IDDataPair} = {};
 
+	/**
+	 * Registers old id and data for new string id.
+	 */
 	export function registerOld(stringId: string, oldId: number, oldData: number): void {
 		oldIDPairs[stringId] = { id: oldId, data: oldData };
 	}
 
+	/**
+	 * Creates ItemStack instance by string id.
+	 * @param stringId new item string id
+	 * @param count item count
+	 * @param data item data
+	 * @param extra item extra data
+	 * @returns ItemStack instance
+	 */
 	export function getStack(stringId: string, count: number = 1, data: number = 0, extra: ItemExtraData = null): ItemStack {
 		if (BlockEngine.getMainGameVersion() == 11) {
 			const oldPair = oldIDPairs[stringId];
@@ -18,24 +32,43 @@ namespace IDConverter {
 				return new ItemStack(oldPair.id, count, oldPair.data, extra);
 			}
 		}
-		return new ItemStack(VanillaItemID[stringId] || VanillaBlockID[stringId], count, data, extra);
+		return new ItemStack(getNumericId(stringId), count, data, extra);
 	}
 
+	/**
+	 * @param stringId new item string id
+	 * @returns converted id data pair
+	 */
 	export function getIDData(stringId: string): IDDataPair {
-		if (BlockEngine.getMainGameVersion() == 11) {
+		if (BlockEngine.getMainGameVersion() == 11 && oldIDPairs.hasOwnProperty(stringId)) {
 			return oldIDPairs[stringId];
-		} else {
-			return { id: VanillaItemID[stringId] || VanillaBlockID[stringId], data: 0 }
 		}
+		return { id: getNumericId(stringId), data: 0 }
 	}
 
+	/**
+	 * @param stringId new item string id
+	 * @returns converted numeric id
+	 */
 	export function getID(stringId: string): number {
-		if (BlockEngine.getMainGameVersion() == 11) return oldIDPairs[stringId].id;
-		else return VanillaItemID[stringId] || VanillaBlockID[stringId];
+		if (BlockEngine.getMainGameVersion() == 11 && oldIDPairs.hasOwnProperty(stringId)) {
+			return oldIDPairs[stringId].id;
+		}
+		return getNumericId(stringId);
 	}
 
+	/**
+	 * @param stringId new item string id
+	 * @returns converted data
+	 */
 	export function getData(stringId: string): number {
-		if (BlockEngine.getMainGameVersion() == 11) return oldIDPairs[stringId].data;
-		else return 0;
+		if (BlockEngine.getMainGameVersion() == 11 && oldIDPairs.hasOwnProperty(stringId)) {
+			return oldIDPairs[stringId].data;
+		}
+		return 0;
+	}
+
+	function getNumericId(stringId: string) {
+		return VanillaItemID[stringId] || VanillaBlockID[stringId];
 	}
 }
