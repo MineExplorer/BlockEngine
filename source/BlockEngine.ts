aa078b2316c1b6cb93c459b0a206da53ab1d038f
MineExplorer <vladgrz40@gmail.com>
2022-12-21 19:33:00 +0300

    helper methods docs

--- source/BlockEngine.ts
+++ source/BlockEngine.ts
@@ -11,14 +11,25 @@ const EntityGetPitch = ModAPI.requireGlobal("Entity.getPitch");
 namespace BlockEngine {
 	const gameVersion = getMCPEVersion().array;
 
+	/**
+	 * @returns game version as array
+	 */
 	export function getGameVersion(): number[] {
 		return gameVersion;
 	}
 
+	/**
+	 * @returns main game version number
+	 */
 	export function getMainGameVersion(): number {
 		return gameVersion[1];
 	}
 
+	/**
+	 * Sends packet with message which will be translated by the client. 
+	 * @param client receiver client
+	 * @param texts array of strings which will be translated and combined in one message.
+	 */
 	export function sendUnlocalizedMessage(client: NetworkClient, ...texts: string[]): void {
 		client.send("blockengine.clientMessage", {texts: texts});
 	}
